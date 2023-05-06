a9b5d3147e88b5157cd359d38fd09060a0754c21
Jamie Temple <jamie-temple@live.de>
2023-05-06 17:21:56 +0200

    create HLA for d3

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,73 @@
 import './style.css'
 import viteLogo from '/vite.svg'
+import "./map/map"
 
-document.querySelector<HTMLDivElement>("body")!.innerHTML = `
-  <div>
-    <h1>Vite + TypeScript</h1>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-  </div>
-`
+const options =
+  ["All staff compensation as % of total expenditure in public institutions (%)",
+    "Arable land (% of total land area)",
+    "Arable land (thousand hectares)",
+    "Assault rate per 100,000 population",
+    "Capital expenditure as % of total expenditure in public institutions (%)",
+    "Changes in stocks (petajoules)",
+    "Current expenditure other than staff compensation as % of total expenditure in public institutions (%)",
+    "Emissions (thousand metric tons of carbon dioxide)",
+    "Emissions per capita (metric tons of carbon dioxide)",
+    "Forest cover (% of total land area)",
+    "Forest cover (thousand hectares)",
+    "GDP in constant 2010 prices (millions of US dollars)",
+    "GDP in current prices (millions of US dollars)",
+    "GDP per capita (US dollars)",
+    "GDP real rates of growth (percent)",
+    "Id_Country_Map",
+    "Important sites for terrestrial biodiversity protected (% of total sites protected)",
+    "Infant mortality for both sexes (per 1,000 live births)",
+    "Intentional homicide rates per 100,000",
+    "Kidnapping at the national level, rate per 100,000",
+    "Labour force participation - Female",
+    "Labour force participation - Male",
+    "Labour force participation - Total",
+    "Land area (thousand hectares)",
+    "Life expectancy at birth for both sexes (years)",
+    "Life expectancy at birth for females (years)",
+    "Life expectancy at birth for males (years)",
+    "Maternal mortality ratio (deaths per 100,000 population)",
+    "Net imports [Imports - Exports - Bunkers] (petajoules)",
+    "Percentage of male and female intentional homicide victims, Female",
+    "Percentage of male and female intentional homicide victims, Male",
+    "Permanent crops (% of total land area)",
+    "Permanent crops (thousand hectares)",
+    "Population aged 0 to 14 years old (percentage)",
+    "Population aged 60+ years old (percentage)",
+    "Population annual rate of increase (percent)",
+    "Population density",
+    "Population mid-year estimates (millions)",
+    "Population mid-year estimates for females (millions)",
+    "Population mid-year estimates for males (millions)",
+    "Primary energy production (petajoules)",
+    "Public expenditure on education (% of GDP)",
+    "Robbery at the national level, rate per 100,000 population",
+    "Safely managed drinking water sources, rural (Proportion of population with access)",
+    "Safely managed drinking water sources, total (Proportion of population with access)",
+    "Safely managed drinking water sources, urban (Proportion of population with access)",
+    "Safely managed sanitation facilities, rural (Proportion of population with access)",
+    "Safely managed sanitation facilities, total (Proportion of population with access)",
+    "Safely managed sanitation facilities, urban (Proportion of population with access)",
+    "Sex ratio (males per 100 females)",
+    "Supply per capita (gigajoules)",
+    "Surface area (thousand km2)",
+    "Theft at the national level, rate per 100,000 population",
+    "Total Sexual Violence at the national level, rate per 100,000",
+    "Total fertility rate (children per women)",
+    "Total supply (petajoules)",
+    "Unemployment rate - Female",
+    "Unemployment rate - Male",
+    "Unemployment rate - Total",]
+
+const dropdown = document.getElementById("dataset-select")
+
+options.forEach(elem => {
+  const opt = document.createElement("option");
+  opt.value = elem;
+  opt.innerText = elem;
+  dropdown?.append(opt);
+})
---
+++ src/map/figure.ts
@@ -0,0 +1,104 @@
+import * as d3 from "d3";
+import { Feature, FeatureCollection, GeoJsonProperties, Geometry } from "geojson";
+
+interface AtlasSVG {
+    HTML: HTMLElement,
+    D3: d3.Selection<d3.BaseType, unknown, HTMLElement, any>,
+}
+
+interface AtlasSettings {
+    pathGenerator: d3.GeoPath<any, d3.GeoPermissibleObjects>,
+    colorScale: d3.ScaleLogarithmic<any, any>,
+    property: string,
+}
+
+type AtlasGeoData = FeatureCollection<Geometry, GeoJsonProperties>;
+
+class Atlas {
+    constructor(domElementId: string, settings: AtlasSettings | null) {
+        const HTML = document.getElementById(domElementId);
+        if (HTML === null)
+            throw Error(`domElement with the id "${domElementId}" does not exist.`);
+        const D3 = d3.select(`#${domElementId}`);
+        D3.append("g");
+        this.svg = { HTML, D3 };
+        this.settings = settings;
+    }
+
+    update(data: AtlasGeoData) {
+        const selection = this.svg.D3.select("g")
+            .selectAll("path")
+            .data(data.features);
+
+        if (this.settings === null) {
+            console.warn("Cannot update without settings");
+            return;
+        }
+
+        selection.join(
+            enter => enter
+                .append("path")
+                .classed("country", true)
+                .style("opacity", 1)
+                .style("stroke-width", ".05vh")
+                // @ts-ignore expects null but sets callback
+                .on("mouseover", this.countryMouseOver)
+                // @ts-ignore expects null but sets callback
+                .on("mouseleave", this.countryMouseLeave)
+
+                .attr("d", this.settings!.pathGenerator)
+                // @ts-ignore cannot infer type
+                .attr("fill", feature => {
+                    return this.transform(feature, this.settings!)
+                }),
+            update => update
+                .attr("d", this.settings!.pathGenerator)
+                // @ts-ignore cannot infer type
+                .attr("fill", feature => {
+                    return this.transform(feature, this.settings!)
+                }),
+            exit => exit
+                .exit()
+                .remove()
+        )
+    }
+
+    private transform(
+        feature: Feature<Geometry, GeoJsonProperties>, settings: AtlasSettings
+    ): string | undefined {
+        if (feature.properties) {
+            if (Number.isNaN(feature.properties[settings.property])) return "#333"
+            return settings.colorScale(feature.properties[settings.property])
+        }
+    }
+
+    private countryMouseOver(d: MouseEvent) {
+        d3.selectAll(".country")
+            .transition()
+            .duration(80)
+            .style("opacity", ".2")
+            .style("stroke-width", ".0")
+
+        // @ts-ignore cannot infer type
+        d3.select(d.target)
+            .transition()
+            .duration(80)
+            .style("opacity", 1)
+            .style("stroke-width", ".12vh")
+    }
+
+    private countryMouseLeave() {
+        d3.selectAll(".country")
+            .transition()
+            .duration(240)
+            .style("opacity", 1)
+            .style("stroke-width", ".05vh")
+    }
+
+    svg: AtlasSVG;
+    settings: AtlasSettings | null;
+}
+
+
+
+export { Atlas };
