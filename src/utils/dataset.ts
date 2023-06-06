7edf4d595c837f35d3a16dc21cd36dd5b374cabb
Jamie Temple <jamie-temple@live.de>
2023-06-06 12:17:45 +0200

    change: make threshold de facto standard

--- src/utils/dataset.ts
+++ src/utils/dataset.ts
@@ -204,23 +204,23 @@ class Dataset {
 }
 
 const COLOUR_MAPPING = new Map<string, Scaling>([
-    ["Arable land (percent of total land area)", { type: "Linear", colourScheme: "Mono", thresholdType: "", thresholds: [] }],
+    ["Arable land (percent of total land area)", { type: "Threshold", colourScheme: "Mono", thresholdType: "", thresholds: [] }],
     ["Emissions per capita (metric tons of carbon dioxide)", { type: "Threshold", colourScheme: "Mono", thresholdType: "", thresholds: [] }],
     ["Forest cover (percent of total land area)", { type: "Threshold", colourScheme: "Mono", thresholdType: "", thresholds: [] }],
-    ["Important sites for terrestrial biodiversity protected (percent of total sites protected)", { type: "Linear", colourScheme: "Mono", thresholdType: "", thresholds: [] }],
-    ["Permanent crops (percent of total land area)", { type: "Logarithmic", colourScheme: "Mono", thresholdType: "Custom", thresholds: [1, 2, 4, 8, 10, Number.MAX_SAFE_INTEGER] }],
+    ["Important sites for terrestrial biodiversity protected (percent of total sites protected)", { type: "Threshold", colourScheme: "Mono", thresholdType: "", thresholds: [] }],
+    ["Permanent crops (percent of total land area)", { type: "Threshold", colourScheme: "Mono", thresholdType: "Custom", thresholds: [1, 2, 4, 8, 10, Number.MAX_SAFE_INTEGER] }],
 
     ["Balance of Payments Current account (millions of US dollars)", { type: "Threshold", colourScheme: "Duo", thresholdType: "Custom", thresholds: [-3000, -1000, 0, 1000, 3000, Number.MAX_SAFE_INTEGER] }],
     ["Balance of Payments Financial account (millions of US dollars)", { type: "Threshold", colourScheme: "Duo", thresholdType: "Custom", thresholds: [-10000, -500, 0, 500, 10000, Number.MAX_SAFE_INTEGER] }],
     ["GDP per capita (US dollars)", { type: "Threshold", colourScheme: "Mono", thresholdType: "Logarithmic", thresholds: [] }],
     ["GDP real rates of growth (percent)", { type: "Threshold", colourScheme: "Duo", thresholdType: "Linear", thresholds: [] }],
-    ["Grants of patents (number)", { type: "Logarithmic", colourScheme: "Mono", thresholdType: "Custom", thresholds: [50, 2000, 8000, 25000, 50000, Number.MAX_SAFE_INTEGER] }],
+    ["Grants of patents (number)", { type: "Threshold", colourScheme: "Mono", thresholdType: "Custom", thresholds: [50, 2000, 8000, 25000, 50000, Number.MAX_SAFE_INTEGER] }],
 
     ["Infant mortality for both sexes (per 1,000 live births)", { type: "Threshold", colourScheme: "Mono", thresholdType: "Custom", thresholds: [5, 10, 20, 40, 80, Number.MAX_SAFE_INTEGER] }],
     ["Life expectancy at birth for both sexes (years)", { type: "Threshold", colourScheme: "Mono", thresholdType: "Custom", thresholds: [60, 65, 70, 75, 80, Number.MAX_SAFE_INTEGER] }],
-    ["Percentage of individuals using the internet", { type: "Logarithmic", colourScheme: "Mono", thresholdType: "", thresholds: [] }],
-    ["Population aged 0 to 14 years old (percentage)", { type: "Linear", colourScheme: "Mono", thresholdType: "", thresholds: [] }],
-    ["Population aged 60+ years old (percentage)", { type: "Linear", colourScheme: "Mono", thresholdType: "", thresholds: [] }],
+    ["Percentage of individuals using the internet", { type: "Threshold", colourScheme: "Mono", thresholdType: "", thresholds: [] }],
+    ["Population aged 0 to 14 years old (percentage)", { type: "Threshold", colourScheme: "Mono", thresholdType: "", thresholds: [] }],
+    ["Population aged 60+ years old (percentage)", { type: "Threshold", colourScheme: "Mono", thresholdType: "", thresholds: [] }],
     ["Population annual rate of increase (percent)", { type: "Threshold", colourScheme: "Duo", thresholdType: "Custom", thresholds: [-2, -1, 0, 2, 4, Number.MAX_SAFE_INTEGER] }],
     ["Population density", { type: "Threshold", colourScheme: "Mono", thresholdType: "Custom", thresholds: [10, 50, 100, 200, 400, Number.MAX_SAFE_INTEGER] }],
     ["Population mid-year estimates (millions)", { type: "Threshold", colourScheme: "Mono", thresholdType: "Custom", thresholds: [1, 10, 100, 200, 500, Number.MAX_SAFE_INTEGER] }],
