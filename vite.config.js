610866bc6c91aed5c9074e062a7e1356c9f0aafa
Jamie Temple <jamie-temple@live.de>
2023-06-07 14:18:29 +0200

    fix: build problem with top level await

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
   build: {
-    target: 'esnext',
+    target: 'es2020',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        // nested: resolve(__dirname, 'nested/index.html'),
+        visualisation: resolve(__dirname, 'pages/visualisation.html'),
+        about: resolve(__dirname, 'pages/about.html'),
       },
     },
   },
+  plugins: [topLevelAwait()],
 })
